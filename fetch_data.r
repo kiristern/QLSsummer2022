01e53d3d32e2f918fccab4d56f706bfdb1108fb6
Stern <Kiri.Stern@inspection.gc.ca>
2022-08-10 08:51:12 -0400

    get spatialLIBD data

--- fetch_data.r
+++ fetch_data.r
@@ -1,24 +1,14 @@
 setwd('Desktop/')
 getwd()
 
-if (!requireNamespace("BiocManager", quietly = TRUE)) {
-    install.packages("BiocManager")
-}
+# if (!requireNamespace("BiocManager", quietly = TRUE)) {
+#     install.packages("BiocManager")
+# }
 
-BiocManager::install("spatialLIBD")
+# BiocManager::install("spatialLIBD")
 
 ## Load the package
 library("spatialLIBD")
-
-## Download the spot-level data
-spe <- fetch_data(type = "spe")
-
-## This is a SpatialExperiment object
-spe
-
-genexspot <- assays(spe)
-genexspot_count <- genexspot$counts
-genexspot_logcount <- genexspot$logcounts
 
 create_csr <- function(mat){
     csr_count <- summary(mat)
@@ -27,23 +17,38 @@ create_csr <- function(mat){
                         count = csr_count$x)
 }
 
+## Download the spot-level data
+spe <- fetch_data(type = "spe")
+
+## This is a SpatialExperiment object
+spe
+
+# investigate data
+gene_meta <- rowData(spe)
+write.csv(gene_meta, "projects/spatialLIBD/data/gene_meta.csv")
+
+# counts matrix
+genexspot <- assays(spe)
+genexspot_count <- genexspot$counts
+genexspot_logcount <- genexspot$logcounts
+
 # create csr
 csr_counts <- create_csr(genexspot_count)
 head(csr_counts)
-write.csv(csr_counts, "Desktop/spatialLIBD_csr_counts.csv")
+write.csv(csr_counts, "projects/spatialLIBD/data/spatialLIBD_csr_counts.csv")
 
 csr_logcounts <- create_csr(genexspot_logcount)
 head(csr_logcounts)
-write.csv(csr_logcounts, file = "Desktop/spatialLIBD_csr_logcounts.csv")
+write.csv(csr_logcounts, file = "projects/spatialLIBD/data/spatialLIBD_csr_logcounts.csv")
 
 # get col metadata
-spot_counts <- colData(spe)
-head(spot_counts)
-write.csv(spot_counts, "Desktop/spatialLIBD_spot_counts.csv")
+spot_meta <- colData(spe)
+head(spot_meta)
+write.csv(spot_meta, "projects/spatialLIBD/data/spatialLIBD_spot_meta.csv")
 
 spot_st <- spatialCoords(spe)
 head(spot_st)
-write.csv(spot_st, "Desktop/spatialLIBD_spot_st.csv")
+write.csv(spot_st, "projects/spatialLIBD/data/spatialLIBD_spot_st.csv")
 
 
 ##### SingleCellExperiment data #####
@@ -54,19 +59,24 @@ genexcell <- assays(sce)
 genexcell_count <- genexcell$counts
 genexcell_logcount <- genexcell$logcounts
 
+# count matrix
 csr_counts_cell <- create_csr(genexcell_count)
 head(csr_counts_cell)
-write.csv(csr_counts_cell, "Desktop/spatialLIBD_csr_counts_cell.csv")
+write.csv(csr_counts_cell, "projects/spatialLIBD/data/spatialLIBD_csr_counts_cell.csv")
 
 csr_logcounts_cell <- create_csr(genexcell_logcount)
 head(csr_logcounts_cell)
-write.csv(csr_logcounts_cell, file = "Desktop/spatialLIBD_csr_logcounts_cell.csv")
+write.csv(csr_logcounts_cell, file = "projects/spatialLIBD/data/spatialLIBD_csr_logcounts_cell.csv")
+
+# gene meta
+gene_meta_sce <- rowData(sce)
+write.csv(gene_meta_sce, "projects/spatialLIBD/data/gene_meta_sce.csv")
 
 # get col metadata
-cell_counts <- colData(sce)
-head(cell_counts)
-write.csv(cell_counts, "Desktop/spatialLIBD_cell_counts.csv")
+cell_meta <- colData(sce)
+head(cell_meta)
+write.csv(cell_meta, "projects/spatialLIBD/data/spatialLIBD_cell_meta.csv")
 
 cell_st <- spatialCoords(sce)
 head(cell_st)
-write.csv(cell_st, "Desktop/spatialLIBD_cell_st.csv")
+write.csv(cell_st, "projects/spatialLIBD/data/spatialLIBD_cell_st.csv")
